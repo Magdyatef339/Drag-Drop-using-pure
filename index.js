c7e0c7264ef90552947b70fa8203e31080bd87d4
Magdy Atef , Frontend Web Developer & MERN STACK Developer <102534938+Magdyatef339@users.noreply.github.com>
2023-01-10 07:00:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+let inp = document.getElementById('inp');
+let btn = document.getElementById('btn');
+let boxs = document.querySelectorAll('.box');
+let drag = null;
+
+
+btn.onclick = function (){
+    if(inp.value != ''){
+        boxs[0].innerHTML += `
+        <p class="item" draggable="true">${inp.value}</p>
+        `
+        inp.value = ''
+    }
+    dragItem()
+}
+function dragItem() {
+    let item = document.querySelectorAll('.item');
+    item.forEach( item=> {
+        item.addEventListener('dragstart', function(){
+            drag = item;
+            item.style.opacity = '.5'
+        })
+        item.addEventListener('dragend', function(){
+            drag = null;
+            item.style.opacity = '1'
+
+        })
+        boxs.forEach(box=> {
+            box.addEventListener('dragover', function(e){
+                e.preventDefault()
+                this.style.background = '#090'
+                this.style.color = '#fff'
+            })
+            box.addEventListener('dragleave', function(){
+                this.style.background = '#fff'
+                this.style.color = '#000'
+            })
+            box.addEventListener('drop', function(){
+                box.append(drag)
+                this.style.background = '#fff'
+                this.style.color = '#000'
+            })
+        })
+    })
+}
